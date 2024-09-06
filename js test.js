c4a6262b610ad6a9c7524b82166e75f4b104cce2
unknown <bhukyasoni298@gmail.com>
2024-09-06 15:26:43 +0530

    first commit

---
+++ js test.js
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <script type="text/javascript">
+        var a=10,b=90;
+        if(a>b)
+        {
+          document.writeln("a is greater")
+        }
+        else
+        {
+        document.writeln("b is greater")
+        }
+        </script>
+    </body>
+</html>
